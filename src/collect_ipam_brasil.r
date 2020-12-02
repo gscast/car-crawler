6564fbb4071c72044c0aa7f7df8039e825ad3476
gabriel <gabriel.sc@hotmail.com>
2020-12-02 12:20:28 -0300

    updtade docstrings

--- src/collect_ipam_brasil.r
+++ src/collect_ipam_brasil.r
@@ -1,54 +1,98 @@
-library("foreign")
-library("tidyverse")
-library("dplyr")
+library(foreign)
+library(tidyverse)
+library(dplyr)
+library(gsubfn)
 
-args <- commandArgs(trailingOnly = TRUE)
+# read the input and output dir_paths
+read_input <- function() {
+    args <- commandArgs(trailingOnly = TRUE)
 
-# read the input and output paths
-if (length(args) < 1) {
-  stop("At least one argument must be supplied (input dir)", call. = FALSE)
-} else if (length(args) == 1) {
-  # default output file
-  args[2] <- args[1]
+    if (length(args) < 1) {
+        stop("At least one argument must be supplied (input dir)",
+            call. = FALSE
+        )
+    } else if (length(args) == 1) {
+        # default output file
+        args[2] <- args[1]
+    }
+
+    dir_path <- args[1]
+    dst_path <- args[2]
+
+    return(list(dirpath = dir_path, dst_path = dst_path))
 }
 
-dir_path <- args[1]
-dst_path <- args[2]
+# get date from dir_path
+extract_date <- function(dir_path) {
+    date_patterns <- c("%Y%m%d", "%Y")
+    date <- NA
 
-# add files matching the given patterns.
-patterns <- c("*AREA_IMOVEL.dbf", "*_Area_imovel_albers.dbf")
-files <- c()
+    for (date_pattern in date_patterns) {
+        date <- as.Date(basename(dir_path), format = date_pattern)
+        if (!is.na(date)) {
+            break
+        }
+    }
 
-for (regex in patterns) {
+    # check the dirname format. it should be a date containing a year
+    if (is.na(date)) {
+        stop(paste(
+            "Could not extract date from the input directory.",
+            dir_path,
+            "should be named with one of the folowwing patterns:",
+            date_pattern
+        ),
+        .call = FALSE
+        )
+    }
 
-    to_append <- list.files(
-        path = dir_path, pattern = regex,
-        full.names = TRUE, recursive = TRUE
-    )
-    files <- append(files, to_append)
+    return(date)
 }
 
-date <- as.Date(basename(dir_path), format = "%Y%m%d")
+# fetch the database files in dir_path matching the valid patterns.
+fetch_dbf <- function(dir_path) {
+    patterns <- c("*AREA_IMOVEL.dbf", "*_Area_imovel_albers.dbf")
+    files <- c()
 
-if (is.na(date)) {
-    stop("Input directory should be a date such as YYYYMMDD")
+    for (regex in patterns) {
+        to_append <- list.files(
+            path = dir_path, pattern = regex,
+            full.names = TRUE, recursive = TRUE
+        )
+        files <- append(files, to_append)
+    }
+
+    if (!length(files)) {
+        stop(
+            "No valid *.dbf files found",
+            .call = FALSE
+        )
+    }
+
+    return(files)
 }
 
-reshape_df <- function(x) {
+user_input <- read_input()
+dir_path <- user_input["dir_path"]
+dst_path <- user_input["dst_path"]
 
-    #get geocode from folder name
+data_registro <- extract_date(dir_path)
+files <- fetch_dbf(dir_path)
+
+# add the register date and geocode in each dbf observation
+reshape_df <- function(file) {
+    city_data <- read.dbf(file, as.is = TRUE) %>%
+        add_column(DATA = data_registro)
+
+    # get geocode from folder name
     geocode <- stringr::str_split(
-        basename(dirname(dirname(x))), "_")[[1]][2]
+        basename(dirname(dirname(x))), "_"
+    )[[1]][2]
 
-    if (is.na(geocode)) {
-        # no geocode found
-        city_data <- read.dbf(x, as.is = TRUE) %>%
-            add_column(DATA = date)
-    } else
-
-        city_data <- read.dbf(x, as.is = TRUE) %>%
-            add_column(geocodigo = geocode) %>%
-            add_column(DATA = date)
+    # geocode found
+    if (!is.na(geocode)) {
+        city_data <- add_column(city_data, geocodigo = geocode)
+    }
 
     return(city_data)
 }
@@ -57,6 +101,11 @@ stacked_df <- lapply(files, reshape_df) %>%
     dplyr::bind_rows()
 
 str(stacked_df)
+
+output_fp <- file.path(dst_path, paste0(basename(dst_path), "_area_imovel.dbf"))
 write.dbf(
     as.data.frame(stacked_df),
-    file.path(dst_path, paste0(basename(dst_path), "_area_imovel.dbf")))
+    output_fp
+)
+
+print("Resulting database: ", output_fp)
--- src/get_oldest_date.r
+++ src/get_oldest_date.r
@@ -1,6 +1,6 @@
-library("foreign")
-library("tidyverse")
-library("dplyr")
+library(foreign)
+library(tidyverse)
+library(dplyr)
 
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -11,32 +11,40 @@ if (length(args) < 1) {
   # default output file
   args[2] <- args[1]
 }
-
 dir_path <- args[1]
 dst_path <- args[2]
 
 # find the files with each year's properties info.
 files <- list.files(
-    path = dir_path, pattern = "*_area_imovel.dbf",
-    full.names = TRUE, recursive = TRUE
+  path = dir_path, pattern = "*_area_imovel.dbf",
+  full.names = TRUE, recursive = TRUE
 )
 
 print(files)
 
 df_list <- lapply(files, read.dbf, as.is = TRUE)
-
 # join the yearly tables.
 df_cod_imovel <- bind_rows(df_list)
 
 # get the oldest date for each property with the same identification.
-cod_imovel_primeira_data <-  df_cod_imovel %>%
-    group_by(COD_IMOVEL) %>%
-    summarise(PRIMEIRA_DATA = min(DATA)) %>%
-    select(COD_IMOVEL, PRIMEIRA_DATA)
+cod_imovel_primeira_data <- df_cod_imovel %>%
+  group_by(COD_IMOVEL) %>%
+  summarise(PRIMEIRA_DATA = min(DATA)) %>%
+  select(COD_IMOVEL, AREA, PRIMEIRA_DATA)
 
 head(cod_imovel_primeira_data)
 
+output_fp <- file.path(
+  dst_path,
+  paste0(
+    basename(dst_path),
+    "_cod_imovel_por_data.dbf"
+  )
+)
+
 write.dbf(
-    as.data.frame(cod_imovel_primeira_data),
-    file.path(dst_path, paste0(
-        basename(dst_path), "_cod_imovel_por_data.dbf")))
+  as.data.frame(cod_imovel_primeira_data),
+  output_fp
+)
+
+print("Resulting database: ", output_fp)
